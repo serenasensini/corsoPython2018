3661892b7e1e9d407006e56ffb58a184f4a96524
Serena Sensini <serena.sensini@gmail.com>
2018-03-16 11:56:49 +0100

    Second lesson - bonus exercise

---
+++ EsercizioB1.1.py
@@ -0,0 +1,7 @@
+paragrafo = "Viviamo in un mondo che ci disorienta con la sua complessità. Vogliamo comprendere ciò che vediamo attorno a noi e chiederci: Qual è la natura dell'universo? Qual è il nostro posto in esso? Da che cosa ha avuto origine l'universo e da dove veniamo noi? "
+
+def stampaParagrafo(testo):
+print testo
+	
+stampaPararrafo()
+
