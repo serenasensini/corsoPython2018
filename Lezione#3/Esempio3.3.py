d58ca49cfd53157d53855f03f8788e9852c0cb36
serenasensini <serena.sensini@gmail.com>
2018-04-04 22:47:48 +0200

    Forth lesson  - fix examples

--- Lezione#3/Esempio3.3.py
+++ Lezione#3/Esempio3.3.py
@@ -2,15 +2,15 @@ class Quadrato:
     
     lato = 0
     
-    def __init__(self, lato):
-        self.lato = lato
+    def __init__(self, nuovoLato):
+        self.lato = nuovoLato
     def __repr__(self):
         return "Quadrato(" + str(self.lato) + ")"
     def perimetro(self):
         return self.lato*4
     def area(self):
         return self.lato*self.lato
-
+       
 q = Quadrato(5)
 print q.perimetro()
 print q.area()
